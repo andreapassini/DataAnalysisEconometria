681d9fcc8f5b0e1b5e539530673a7f9f11d33248
Andrea Passini <andreapa98@gmail.com>
2022-04-06 19:17:39 +0200

    Update intermedio.R

--- Intermedio/intermedio.R
+++ Intermedio/intermedio.R
@@ -1,12 +1,12 @@
 
-rm(list = ls()) # PERCHE'?
+rm(list = ls())
 
 library(MASS)
 library(dplyr)
 library(ggplot2)
 library(AER)
 
-load("Data_takehome_2.RData")
+load("C:/Users/Nicola/Desktop/intermedio econometria/Data_takehome_2.RData")
 
 dat
 summary(dat)
@@ -20,22 +20,86 @@ help(lm)
 #help(dat)
 
 dat <- dat %>%
-  mutate(#greentech_log = log(green_tech),
+  mutate(greentech_log = log(green_tech %>% inf.omit())  ,
         popul_log = log(popul),
         vamanuf_log = log(va_manuf),
  #        rile_log = log(rile), #fixare, valori negativi
         qoi_log = log(qoi))
 
-lm.fit <- lm(vamanuf_log ~ popul_log, data = dat)
-
+lm.fit <- lm(qoi ~ greentech_log, data = dat)
 summary(lm.fit)
 
-ggplot(dat, aes(x = vamanuf_log, y = popul_log)) +
+ggplot(dat, aes(x = qoi, y = greentech_log)) +
   geom_point() +
-  stat_smooth(method = "lm", se = FALSE, size = 1) +
+  stat_smooth(method = "lm", se = TRUE, size = 1) +
   labs(x = "x (log)", 
        y = "y (log)",
        title = "linear regression line")
 
-help("predict")
-predict(lm.fit, data.frame(vamanuf_log = 15), interval = "prediction")
+library(rgl)
+
+x <- dat$rile
+y <- dat$qoi
+z <- log(dat$green_tech)
+
+fit <- lm(y ~ x + z)
+
+grid.lines = 26
+x.pred <- seq(min(x), max(x), length.out = grid.lines)   # Sequence of x values; check by typing in console x.pred
+y.pred <- seq(min(y), max(y), length.out = grid.lines)   # Idem for y values
+xy <- expand.grid(x = x.pred, y = y.pred)                # Create a grid with x and y values (for each y value, we attach the different x values - these will be the 'observations' that we use below)
+z.pred <- matrix(predict(fit, newdata = xy),             # Predict the z values based on the values in the grid, and save the outcome in a matrix
+                 nrow = grid.lines, ncol = grid.lines)
+
+# Run persp3 + point3d if you wish to move the object in 3D
+persp3d(x.pred, y.pred, z.pred, 
+        zlim = c(10, 13),
+        col = "green", expand = 0.8, alpha = 0.5, ticktype = "detailed",
+        xlab = "rile", ylab = "qoi", zlab = "green tech")
+
+points3d(x, y, z, col = "red", size = 5, add = T)
+
+
+rm(pred_int)
+
+#help("predict")
+predict(lm.fit, data.frame(popul_log = 3.5), interval = "prediction")
+
+help(plot.lm)
+plot(lm.fit, which = 1)  
+plot(lm.fit, which = 2)
+
+popul_log = beta0 + beta1*vamanuf_log + beta2*vamanuf_log + u
+lm.multiple <- lm(popul_log ~ vamanuf_log + vamanuf_log, data = dat)
+summary(lm.multiple)
+
+library(rgl)
+
+x <- dat$year
+y <- dat$popul_log
+z <- dat$vamanuf_log
+
+fit <- lm(z ~ x + y)
+
+grid.lines = 26
+x.pred <- seq(min(x), max(x), length.out = grid.lines)
+y.pred <- seq(min(y), max(y), length.out = grid.lines) 
+xy <- expand.grid(x = x.pred, y = y.pred) 
+.pred <- matrix(predict(fit, newdata = xy),             
+                nrow = grid.lines, ncol = grid.lines)
+
+persp3d(x.pred, y.pred, z.pred, 
+        zlim = c(10, 13),
+        col = "year", expand = 0.8, alpha = 0.5, ticktype = "riled",
+        xlab = "va_manuf", ylab = "popular", zlab = "green_tech")
+
+points3d(x, y, z, col = "red", size = 5, add = T)
+
+lpm_1 <- lm(urban ~ green_tech, data = dat)
+summary(lpm_1)
+
+# and calculate robust standard errors since we do detect heteroscedasticity
+bptest(lpm_1)
+coeftest(lpm_1, vcov = vcovHC, type = "HC1")
+
+predict(lpm_1, data.frame(ghg_log = (c(0.1, 1, 2))), interval = "prediction")
